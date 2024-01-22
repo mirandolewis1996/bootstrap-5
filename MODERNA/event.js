50e5bdf6bf36b0870315c6566015078e0e443a0e
mirandolewis1996 <lewismerindo800@gmail.com>
2024-01-22 21:21:32 +0530

    MODERNA BS5

---
+++ MODERNA/event.js
@@ -0,0 +1,12 @@
+const cli = document.getElementById('c1')
+
+const navb = document.getElementById('c2')
+
+
+cli.addEventListener("click",function(){
+   
+     navb.classList.toggle("action");
+
+     cli.classList.toggle("action")
+     
+})
